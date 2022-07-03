c21b051da31a8571f37d360556773db2efded648
Naomi Cole <naomisarahcole@gmail.com>
2022-07-03 14:09:08 +1000

    3 passing test

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ class Circle {
     constructor(radius){
         this.radius = radius;
     }
-    
+    get diameter (){
+        return this.radius * 2;
+    }
+    get circumference (){
+        return (this.radius * 2) * Math.PI;
+    }
 }
