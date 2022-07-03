4f62c17c60ac424bae0c56bd1048b9a9adf3e595
Naomi Cole <naomisarahcole@gmail.com>
2022-07-03 14:14:30 +1000

    Completed assignemnt

--- index.js
+++ index.js
@@ -13,4 +13,10 @@ class Circle {
     get area() {
         return this.radius * this.radius * Math.PI;
     }
+    set diameter(diameter){
+        this.radius = diameter / 2;
+    }
+    set circumference(circumference){
+        this.radius = circumference / (Math.PI * 2);
+    }
 }
